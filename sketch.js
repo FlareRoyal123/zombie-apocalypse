d83f94f439395498d9cc75892eafe788fd15ce26
FlareRoyal123 <83412363+FlareRoyal123@users.noreply.github.com>
2021-05-26 16:19:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,89 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody;
+var groundSprite , ground , grdImg;
+var moon , chanda;
+var cloud , baadal;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+	grdImg = loadImage("grnd.png")
+	chanda = loadImage("moon.png")
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+
+
+	groundSprite=createSprite(width/2, height-10, width,10);
+	groundSprite.addImage("g",grdImg)
+	groundSprite.shapeColor=color(255)
+	groundSprite.scale = 0.2;
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	moon = createSprite(700,100,10,10);
+	moon.addImage(chanda);
+	moon.scale = 0.15;
+
+	
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic: true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic: true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  if(keyDown(DOWN_ARROW)){
+
+	pfall();
+
+  }
+  drawSprites();
+ 
+}
+
+function pfall() {
+ if (keyDown (DOWN_ARROW)) {
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic: true} );
+	World.add(world, ground);
+	
+    packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:false});
+	World.add(world, packageBody);
+  }
+}
+
+
+
